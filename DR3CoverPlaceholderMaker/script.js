fea583e405dcc4689219b95f340370a4f3473ed3
Greenball233 <3120393927@qq.com>
2023-03-24 21:54:02 +0800

    1111

--- DR3CoverPlaceholderMaker/script.js
+++ DR3CoverPlaceholderMaker/script.js
@@ -4,12 +4,37 @@ const startButton = document.getElementById("start-button");
 const coverFileInput = document.getElementById("cover-file");
 const songNameInput = document.getElementById("song-name");
 const songArtistInput = document.getElementById("song-artist");
+const songNameSizeInput = document.getElementById("song-name-size");
+const songArtistSizeInput = document.getElementById("song-artist-size");
+const songNamePosInput = document.getElementById("song-name-pos");
+const songArtistPosInput = document.getElementById("song-artist-pos");
 const downloadButton = document.getElementById("download");
 
 window.addEventListener("load", () => {
     Preload();
     drawPlaceholder();
 });
+songNameSizeInput.addEventListener("change", () => {
+    if (songNameSizeInput.value === "") songNameSizeInput.value = 42;
+    let number = parseInt(songNameSizeInput.value);
+    if (number < 1) number = 1;
+    songNameSizeInput.value = number.toString()
+})
+
+songArtistSizeInput.addEventListener("change", () => {
+    if (songArtistSizeInput.value === "") songArtistSizeInput.value = 37;
+    let number = parseInt(songArtistSizeInput.value);
+    if (number < 1) number = 1;
+    songArtistSizeInput.value = number.toString()
+})
+
+songNamePosInput.addEventListener("change", () => {
+    if (songNamePosInput.value === "") songNamePosInput.value = 100;
+})
+
+songArtistPosInput.addEventListener("change", () => {
+    if (songArtistPosInput.value === "") songArtistPosInput.value = 200;
+})
 
 startButton.addEventListener("click", draw);
 downloadButton.addEventListener("click", download);
@@ -34,13 +59,13 @@ function drawText() {
     textCanvas.height = canvas.height;
     textCanvas.width = canvas.width;
     let ctx1 = textCanvas.getContext("2d");
-    ctx1.font = "70px HGR";
+    ctx1.font = songNameSizeInput.value + "px HGR";
     ctx1.fillStyle = "#FFF";
     ctx1.textAlign = "center";
-    ctx1.fillText(songNameInput.value, 256, 156);
+    ctx1.fillText(songNameInput.value, 150, parseFloat(songNamePosInput.value));
     ctx1.save();
-    ctx1.font = "60px HGR";
-    ctx1.fillText(songArtistInput.value, 256, 356);
+    ctx1.font = songArtistSizeInput.value + "px HGR";
+    ctx1.fillText(songArtistInput.value, 150, parseFloat(songArtistPosInput.value));
     let textImage = new Image();
     textImage.src = textCanvas.toDataURL("image/png");
     textImage.onload = function () {
@@ -65,8 +90,8 @@ function drawPlaceholder() {
     ctx.textBaseline = "middle";
     ctx.textAlign = "center";
     ctx.fillStyle = "#1f1e33";
-    ctx.font = "48px Arial";
-    ctx.fillText(getPlaceholderText(document.documentElement.lang), canvas.width / 2, canvas.height / 2);
+    ctx.font = "28px Arial";
+    ctx.fillText(getPlaceholderText(document.documentElement.lang), 150, 150);
 }
 function download() {
     const downloadElement = document.createElement("a");
